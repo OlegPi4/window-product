f5446750475b379334e534dda5badd5de5fe8b22
OlegPichkur <oleg1pichkur@gmail.com>
2024-09-10 13:50:39 +0300

    01 создан проект

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+/******/ (() => { // webpackBootstrap
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+console.log("55");
+/******/ })()
+;
+//# sourceMappingURL=script.js.map
