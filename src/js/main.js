70cf995214cc0b8ceaff50a8525dfbd98d9b64dc
OlegPichkur <oleg1pichkur@gmail.com>
2024-09-12 18:17:23 +0300

    02 +табы + модальные окна + форма + нач. ф.кальк

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,24 @@
-console.log("55");
+import "./slider";
+import modals from "./modules/modals";
+import tabs from "./modules/tabs";
+import forms from "./modules/forms";
+
+window.addEventListener("DOMContentLoaded", () => {
+  "use strict";
+  modals();
+  tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active");
+  tabs(
+    ".decoration_slider",
+    ".no_click",
+    ".decoration_content > div > div",
+    "after_click"
+  );
+  tabs(
+    ".popup_calc",
+    ".balcon_icons_img",
+    ".popup_calc_content > .big_img > img",
+    "do_image_more",
+    "inline-block"
+  );
+  forms();
+});
