ddbbba739411a6a9a20f4c031e5884790bbe735a
OlegPichkur <oleg1pichkur@gmail.com>
2024-09-13 08:50:44 +0300

    03 форма калькулятор ч,1

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,27 +1,42 @@
 const modals = () => {
-  function bindModal(triggerSelector, modalSelector, closeSelector) {
+  const windows = document.querySelectorAll("[data-modal]");
+  function bindModal(
+    triggerSelector,
+    modalSelector,
+    closeSelector,
+    closeClickOverlay = true
+  ) {
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelector(closeSelector);
+
+    function closeAllModals() {
+      windows.forEach((item) => {
+        item.style.display = "none";
+      });
+    }
 
     trigger.forEach((item) => {
       item.addEventListener("click", (e) => {
         if (e.target) {
           e.preventDefault();
         }
+        closeAllModals();
         modal.style.display = "block";
         document.body.style.overflow = "hidden";
         //document.body.classList.add("modal-open");
       });
     });
     close.addEventListener("click", () => {
-      modal.style.display = "none";
+      //modal.style.display = "none";
+      closeAllModals();
       document.body.style.overflow = "";
       //document.body.classList.remove("modal-open");
     });
     modal.addEventListener("click", (e) => {
-      if (e.target === modal) {
-        modal.style.display = "none";
+      if (e.target === modal && closeClickOverlay) {
+        //modal.style.display = "none";
+        closeAllModals();
         document.body.style.overflow = "";
         //document.body.classList.remove("modal-open");
       }
@@ -35,6 +50,12 @@ const modals = () => {
     }, time);
   }
 
+  function addClassesAnimation() {
+    windows.forEach((item) => {
+      item.classList.add("animate__animated", "animate__pulse");
+    });
+  }
+
   bindModal(
     ".popup_engineer_btn",
     ".popup_engineer",
@@ -42,7 +63,21 @@ const modals = () => {
   );
   bindModal(".phone_link", ".popup", ".popup .popup_close");
   bindModal(".popup_calc_btn", ".popup_calc", ".popup_calc_close");
+  bindModal(
+    ".popup_calc_button",
+    ".popup_calc_profile",
+    ".popup_calc_profile_close",
+    false
+  );
+  bindModal(
+    ".popup_calc_profile_button",
+    ".popup_calc_end",
+    ".popup_calc_end_close",
+    false
+  );
+
   showModalByTime(".popup", 60000);
+  addClassesAnimation();
 };
 
 export default modals;
